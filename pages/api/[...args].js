dc5a48eb8079b9de8ab0e0083d906a285d1642cc
Matt Roh <hjroh0315alt@gmail.com>
2025-11-11 11:37:21 +0900

    Delete [...args].js

--- pages/api/[...args].js
+++
@@ -1,9 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export default createProxyMiddleware({
-      target: 'https://solved.ac/api/v3',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '',
-      },prependPath: true,
-    });
